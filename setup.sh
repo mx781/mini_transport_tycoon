fc896a24fca1f62bff55bdddcfdbe5fd38b66150
Max Knutins <max@qqmx.eu>
2016-11-28 20:43:04 -0500

    Setup.sh, readme update.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+sudo apt-get install gtk2.0
 opam install graphics ocamlgraph camlimages lablgtk piqi
 sudo apt-get install libpng12-dev libjpeg-dev libtiff-dev libxpm-dev libfreetype6-dev libgif-dev
 clear
