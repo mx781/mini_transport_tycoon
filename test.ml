bfa2c848cf0421c99610c8f3e6ef476941ba962a
Max Knutins <max@qqmx.eu>
2016-12-04 17:16:39 -0500

    Finalized testing of handle_processes.

--- test.ml
+++ test.ml
@@ -1,7 +1,13 @@
 open OUnit2
 open GameElements
+open InputProcessing
 open Player
 open Engine
+
+(* Tuple helpers *)
+let f (x, _, _) = x
+let s (_,y,_) = y
+let t (_,_,z) = z
 
 (* *************** *)
 (* Gamestate tests *)
@@ -33,8 +39,16 @@ let truck1 = {
   v_loc = Some 1;
 }
 
+let car2 = {
+  v_owner_id = 0; speed = 4.; capacity = 25; v_t = Car;
+  cargo = Some {t = Lumber; quantity = 25}; age = 37; status = Waiting;
+  x = 510.; y = 320.; destination = []; v_loc = Some 0;
+}
+
 let gs1 = DataProcessing.load_file "tests/gs1.json"
 let gs1_addroad1 = DataProcessing.load_file "tests/gs1_addroad1.json"
+let gs2 = DataProcessing.load_file "tests/gs2.json"
+let gs2_buycargo1 = DataProcessing.load_file "tests/gs2_buycargo1.json"
 let road1 = 
 {
   c_owner_id = 0;
@@ -53,25 +67,58 @@ let road2 =
   c_age = 0;
   c_speed = 10.;
 }
+let road3 = 
+{
+  c_owner_id = -1;
+  l_start = 5;
+  l_end = 8;
+  length = 158.1;
+  c_age = 0;
+  c_speed = 3.0;
+}
+let road4 = 
+{
+  c_owner_id = -1;
+  l_start = 6;
+  l_end = 7;
+  length = 186.8;
+  c_age = 0;
+  c_speed = 3.0;
+}
 
 let gs1_test1 = handle_processes [AddRoad(road1)] gs1 false
 let gs1_test2 = handle_processes [AddRoad(road2)] gs1 false
+let gs1_test3 = handle_processes [DeleteRoad(road1)] gs1_addroad1 false
+let gs1_test4 = handle_processes 
+  [PurchaseRoad({road3 with c_owner_id = 0})] gs1 false
+let gs1_test5 = handle_processes
+  [PurchaseRoad({road3 with c_owner_id = 0});
+  PurchaseRoad({road4 with c_owner_id = 0})] gs1 false
+let gs1_test6 = handle_processes
+  [PurchaseRoad({road4 with c_owner_id = 0});
+  PurchaseRoad({road3 with c_owner_id = 0})] gs1 false
+let gs1_test7 = handle_processes [Pause] gs1 false
+let gs1_test8 = handle_processes [Pause;Pause] gs1 false
+let gs1_test9 = handle_processes [PurchaseRoad({road3 with c_owner_id = 0});
+  PurchaseRoad({road3 with c_owner_id = 0})] gs1 false
+
+let gs2_test1 = handle_processes [BuyVehicleCargo(car2)] gs2 false
 
 let gamestate_tests = [
-  "handle_processes: empty"  >:: (fun _ -> assert_equal
+  "handle_processes: empty" >:: (fun _ -> assert_equal
     (handle_processes [] generic_gs false)
     generic_gs
   );
-  "handle_processes: buy car"  >:: (fun _ -> assert_equal
+  "handle_processes: buy car" >:: (fun _ -> assert_equal
     (handle_processes [BuyVehicle(car1)] generic_gs false)
     {generic_gs with vehicles = [car1]; players = [{generic_pl with money =
       (generic_pl.money -. car_price)}]}
   );
-  "handle_processes: buy car, can't afford"  >:: (fun _ -> assert_equal
+  "handle_processes: buy car, can't afford" >:: (fun _ -> assert_equal
     (handle_processes [BuyVehicle(car1)] generic_gs_broke false)
     generic_gs_broke
   );
-  "handle_processes: buy truck + car"  >:: (fun _ -> assert_equal
+  "handle_processes: buy truck + car" >:: (fun _ -> assert_equal
     (handle_processes
       [BuyVehicle(truck1); BuyVehicle(car1)] generic_gs false)
     {generic_gs with
@@ -81,14 +128,14 @@ let gamestate_tests = [
     }
   );
 
-  "handle_processes: buy car, then sell"  >:: (fun _ -> assert_equal
+  "handle_processes: buy car, then sell" >:: (fun _ -> assert_equal
     (handle_processes [BuyVehicle(car1);SellVehicle(car1)] generic_gs false)
     {generic_gs with
       players = [{generic_pl with money =
         (generic_pl.money -. ((1. -. sell_back_percentage) *. car_price))}]
     }
   );
-  "handle_processes: sell vehicle you don't own"  >:: (fun _ -> assert_equal
+  "handle_processes: sell vehicle you don't own" >:: (fun _ -> assert_equal
     (handle_processes [SellVehicle(truck1)] generic_gs false)
     {generic_gs with
       players = [{generic_pl with money =
@@ -96,36 +143,95 @@ let gamestate_tests = [
     }
   );
 
-  "handle_processes: add road"  >:: (fun _ -> assert_equal
+  "handle_processes: add road" >:: (fun _ -> assert_equal
     (Map.find_edge gs1_test1.graph
         (get_loc 6 gs1_test1.graph) (get_loc 10 gs1_test1.graph))
     (Map.find_edge gs1_addroad1.graph
         (get_loc 6 gs1_addroad1.graph) (get_loc 10 gs1_addroad1.graph))
   );
-  "handle_processes: add road unaffordable"  >:: (fun _ -> assert_raises
+  "handle_processes: add road unaffordable" >:: (fun _ -> assert_raises
     Not_found
     (fun _ -> (Map.find_edge gs1_test2.graph
         (get_loc 4 gs1_test2.graph) (get_loc 10 gs1_test2.graph)))
   );
 
-  "handle_processes: sell road"  >:: (fun _ -> assert_equal
-    (Map.find_edge gs1_test1.graph
-        (get_loc 6 gs1_test1.graph) (get_loc 10 gs1_test1.graph))
-    (Map.find_edge gs1_addroad1.graph
-        (get_loc 6 gs1_addroad1.graph) (get_loc 10 gs1_addroad1.graph))
+  "handle_processes: delete road" >:: (fun _ -> assert_raises
+    Not_found
+    (fun _ -> (Map.find_edge gs1_test3.graph
+        (get_loc 6 gs1_test3.graph) (get_loc 10 gs1_test3.graph)))
+  );
+  "handle_processes: delete unexisting road" >:: (fun _ -> assert_raises
+    Not_found
+    (fun _ -> handle_processes [DeleteRoad(road2)] gs1_addroad1 false)
   );
 
-  (*SetVehicleDestination(v)::t -> handle_processes t (set_v_dest v st) road_bought
-    | BuyVehicleCargo(v)::t -> handle_processes t (set_v_cargo v st) road_bought
-    | AddRoad(c)::t ->
-        handle_processes t (if road_bought then st else buy_connection c st) true
-    | DeleteRoad(c)::t -> handle_processes t (sell_connection c st) road_bought
-    | PurchaseRoad(c)::t ->
-        handle_processes t (if road_bought then st else change_connection_owner c st) true
-    | Pause::t-> handle_processes t ({st with paused = not st.paused}) road_bought
-    | EndGame::t -> raise EndGame
-    | Nothing::t -> handle_processes t st road_bought
-  *)
+  "handle_processes: purchase road" >:: (fun _ -> assert_equal
+    (let e = Map.find_edge gs1_test4.graph 
+      (get_loc 5 gs1_test4.graph) (get_loc 8 gs1_test4.graph)
+    in (s e).c_owner_id)
+    0
+  );
+  "handle_processes: purch. 2rds A - 1st purchased" >:: (fun _ -> assert_equal
+    (let e = Map.find_edge gs1_test5.graph 
+      (get_loc 5 gs1_test5.graph) (get_loc 8 gs1_test5.graph)
+    in (s e).c_owner_id)
+    0
+  );
+  "handle_processes: purch. 2rds A - 2nd unaffordable" >:: (fun _ ->assert_equal
+    (let e = Map.find_edge gs1_test5.graph 
+      (get_loc 6 gs1_test5.graph) (get_loc 7 gs1_test5.graph)
+    in (s e).c_owner_id)
+    ~-1
+  );
+  "handle_processes: purch. 2rds B - 1st purchased" >:: (fun _ -> assert_equal
+    (let e = Map.find_edge gs1_test6.graph 
+      (get_loc 6 gs1_test5.graph) (get_loc 7 gs1_test5.graph)
+    in (s e).c_owner_id)
+    0
+  );
+  "handle_processes: purch. 2rds B - 2nd unaffordable" >:: (fun _ ->assert_equal
+    (let e = Map.find_edge gs1_test6.graph 
+      (get_loc 5 gs1_test6.graph) (get_loc 8 gs1_test6.graph)
+    in (s e).c_owner_id)
+    ~-1
+  );
+  "handle_processes: purchase same road twice: road" >:: (fun _ -> assert_equal
+    (let e = Map.find_edge gs1_test9.graph 
+      (get_loc 5 gs1_test9.graph) (get_loc 8 gs1_test9.graph)
+    in (s e).c_owner_id)
+    0
+  );
+
+  "handle_processes: pause" >:: (fun _ -> assert_equal
+    gs1_test7
+    {gs1 with paused = true}
+  );
+  "handle_processes: pause/unpause" >:: (fun _ -> assert_equal
+    gs1_test8
+    gs1
+  );
+
+  "handle_processes: nothing" >:: (fun _ -> assert_equal
+    (handle_processes [Nothing] gs1 false)
+    gs1
+  );
+  "handle_processes: nothing list" >:: (fun _ -> assert_equal
+    (handle_processes [Nothing;Nothing;Nothing;Nothing;Nothing;] gs1 false)
+    gs1
+  );
+
+  "handle_processes: buy cargo - vehicle" >:: (fun _ -> assert_equal
+    gs2_test1.vehicles
+    gs2_buycargo1.vehicles
+  );
+  "handle_processes: buy cargo - player money" >:: (fun _ -> assert_equal
+    gs2_test1.players
+    gs2_buycargo1.players
+  );
+  "handle_processes: buy cargo - edge" >:: (fun _ -> assert_equal
+    (get_loc 0 gs2_test1.graph)
+    (get_loc 0 gs2_buycargo1.graph)
+  );
 
 ]
 
